0278479f8fbf513224f8d254304536b82a444672
olid16 <olid16@gmail.com>
2014-12-17 16:01:50 +0000

    Add skeleton for first cucumber test

---
+++ user-service-acceptance/src/test/java/CreateEmployerStepDefs.java
@@ -0,0 +1,24 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CreateEmployerStepDefs {
+    @Then("^an employer is created$")
+    public void an_employer_is_created() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Given("^User fills employer data$")
+    public void User_fills_employer_data() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^the user create an employer$")
+    public void the_user_create_an_employer() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+}
---
+++ user-service-acceptance/src/test/resources/CreateEmployer.feature
@@ -0,0 +1,9 @@
+Feature: Create Employer
+  As a user
+  I want to create an employer
+  So I can manage jobs and jobs applications
+
+  Scenario: Create Employer
+    Given User fills employer data
+    When the user create an employer
+    Then an employer is created
